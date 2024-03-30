57c8bb744c95d1d8ded2f31c7321e53b4e0736ac
Natansh05 <natansh.me05@gmail.com>
2024-03-30 14:21:26 +0530

    added border to each item in menu

--- lib/src/common widgets/my_food_tile.dart
+++ lib/src/common widgets/my_food_tile.dart
@@ -19,9 +19,12 @@ class FoodTile extends StatelessWidget {
         GestureDetector(
           onTap: onTap,
           child: Container(
+            margin: EdgeInsets.all(10.0),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(12.0),
-
+              border: Border.all(
+                color: Theme.of(context).colorScheme.secondary,
+              ),
             ),
             child: Padding(
               padding: const EdgeInsets.all(15.0),
@@ -36,6 +39,9 @@ class FoodTile extends StatelessWidget {
                           style: TextStyle(
                             color: Theme.of(context).colorScheme.primary,
                           ),),
+                          const SizedBox(
+                            height: 10.0,
+                          ),
                           Text(food.description),
                         ],
                       ),
