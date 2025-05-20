b74eb2fb2b26060b072ebe0bd6e1ada247bdba9f
Natansh05 <natansh.me05@gmail.com>
2025-05-20 13:28:42 +0530

    comeback initialised

---
+++ lib/src/pages/edit_profile_page.dart
@@ -0,0 +1,164 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:myapp/Services/auth/auth_service.dart';
+
+class EditProfilePage extends StatefulWidget {
+  @override
+  State<EditProfilePage> createState() => _EditProfilePageState();
+}
+
+class _EditProfilePageState extends State<EditProfilePage> {
+  final AuthService _authService = AuthService();
+  User? user;
+  late String userEmail = "";
+  late String userPhone = "";
+  late String userAdress = "";
+  late String userName = "";
+  // Initial values for the profile fields
+  final TextEditingController nameController = TextEditingController(text: "John Doe");
+  final TextEditingController emailController = TextEditingController(text: "john.doe@example.com");
+  final TextEditingController phoneController = TextEditingController(text: "Not Specified Yet");
+  final TextEditingController addressController = TextEditingController(text: "Not Specified Yet");
+
+  @override
+  void initState() {
+    super.initState();
+    user = _authService.getCurrentUser();
+    if (user != null) { // Ensure user is not null before accessing properties
+      userEmail = user!.email ?? 'No email available';
+      userPhone = user!.phoneNumber ?? 'No phone number available';
+      userName = user!.displayName ?? 'No name available';
+      emailController.text = userEmail; // Set email controller to user email
+      nameController.text = user!.displayName ?? 'No name given';
+    } else {
+      userEmail = 'No email available';
+      userPhone = 'No phone number available';
+      userName = 'No name available';
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      appBar: AppBar(
+        title: Text(
+          'E D I T  P R O F I L E',
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).colorScheme.background,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            _buildProfileHeader(context),
+            SizedBox(height: 16),
+            _buildProfileForm(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildProfileHeader(BuildContext context) {
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        CircleAvatar(
+          radius: 75,
+          backgroundImage: AssetImage('lib/assets/profile_picture.png'),
+          onBackgroundImageError: (exception, stackTrace) {
+            print('Error loading image: $exception');
+          },
+        ),
+        Positioned(
+          bottom: 0,
+          right: 0,
+          child: GestureDetector(
+            onTap: () {
+              // Add functionality to change the profile picture
+            },
+            child: CircleAvatar(
+              backgroundColor: Theme.of(context).colorScheme.secondary,
+              radius: 20,
+              child: Icon(
+                Icons.camera_alt,
+                color: Theme.of(context).colorScheme.onPrimary,
+                size: 20,
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildProfileForm(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          _buildTextField('Full Name', nameController, Icons.person),
+          SizedBox(height: 10),
+          _buildTextField('E-Mail', emailController, Icons.email),
+          SizedBox(height: 10),
+          _buildTextField('Phone No', phoneController, Icons.phone),
+          SizedBox(height: 10),
+          _buildTextField('Default Address', addressController, Icons.home),
+          SizedBox(height: 20),
+          ElevatedButton(
+            onPressed: () {
+              // Implement update logic here
+              // Example: updateUserProfile(nameController.text, emailController.text, phoneController.text, addressController.text);
+            },
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Theme.of(context).colorScheme.background,
+              foregroundColor: Theme.of(context).colorScheme.primary,
+              padding: EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+              textStyle: TextStyle(fontSize: 16),
+            ),
+            child: Text('Edit Profile'),
+          ),
+          SizedBox(height: 20),
+          Text(
+            'Joined 31 October 2022',
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.onPrimary.withOpacity(0.7),
+            ),
+          ),
+          SizedBox(height: 20),
+          TextButton(
+            onPressed: () {
+              // Implement delete account logic here
+            },
+            style: TextButton.styleFrom(
+              foregroundColor: Colors.red,
+            ),
+            child: Text('Delete'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextField(String label, TextEditingController controller, IconData icon) {
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: label,
+        prefixIcon: Icon(icon, color: Theme.of(context).colorScheme.primary),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(30.0),
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+        ),
+      ),
+    );
+  }
+}
