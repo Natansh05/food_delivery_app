02aaff5752ad2a9515751036b8cb8b5f830baf05
Natansh05 <natansh.me05@gmail.com>
2025-05-24 16:49:41 +0530

    Removed some useless bugs

--- lib/src/pages/delivery_page.dart
+++ lib/src/pages/delivery_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:myapp/Services/database/firestore.dart';
 import 'package:myapp/src/common widgets/my_receipt.dart';
--- lib/src/pages/profile_page.dart
+++ lib/src/pages/profile_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
 import 'package:myapp/Services/auth/auth_service.dart';
 import 'package:myapp/src/pages/edit_profile_page.dart';
